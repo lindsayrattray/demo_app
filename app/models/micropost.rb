62d600992a443ff8de82ffc99c55c651013ece03
Lindsay Rattray <lindsayrattray@yahoo.com>
2012-10-05 14:08:26 +1000

    Finish demo app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,7 @@
 class Micropost < ActiveRecord::Base
-	attr_accessible :content, :user_id
-	validates :content, :length => { :maximum => 140 }
+  attr_accessible :content, :user_id
+
+  belongs_to :user
+
+  validates :content, :length => { :maximum => 140 }
 end
